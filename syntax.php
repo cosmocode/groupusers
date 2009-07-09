865747c04891c100a843bbacbc0d5a40b650149f
Andreas Gohr <gohr@cosmocode.de>
2009-07-09 11:05:03 +0200

    upped version info

--- syntax.php
+++ syntax.php
@@ -22,7 +22,7 @@ class syntax_plugin_groupusers extends DokuWiki_Syntax_Plugin {
         return array(
             'author' => 'Dominik Eckelmann',
             'email'  => 'dokuwiki@cosmocode.de',
-            'date'   => '2008-10-06',
+            'date'   => '2009-07-09',
             'name'   => 'Groupusers Syntax plugin',
             'desc'   => 'Displays the users from one or more groups.',
             'url'    => 'http://www.dokuwiki.org/plugin:groupusers'
