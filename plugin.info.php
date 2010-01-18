36f49e6b07681bcbaa5e6b3e20f2f5bdfff087b4
Dominik Eckelmann <eckelmann@karsten.cosmo>
2010-01-18 13:08:35 +0100

    added plugin informations

---
+++ plugin.info.php
@@ -0,0 +1,7 @@
+base    groupusers
+author  Dominik Eckelmann
+email   dokuwiki@cosmocode.de
+date    2009-07-09
+name    Groupusers Syntax plugin
+desc    Displays the users from one or more groups.
+url     http://www.dokuwiki.org/plugin:groupusers
